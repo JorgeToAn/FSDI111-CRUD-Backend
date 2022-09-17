a3362b1d933a616d160ebc3e7f916afb6d346078
JorgeToAn <jorgetosta.f@gmail.com>
2022-09-16 20:29:13 -0700

    Added remaining data types info

--- app/routes.py
+++ app/routes.py
@@ -4,6 +4,8 @@ app = Flask(__name__)
 
 from app.database import data_types
 
+
+# ---------- GET ------------
 @app.get("/data_types")
 def index():
     out = data_types.scan()
@@ -24,8 +26,36 @@ def booleans():
     out = data_types.select_by_type(name="Boolean")
     return out
 
+@app.get("/data_types/string")
+def string():
+    out = data_types.select_by_type(name="String")
+    return out
+
+@app.get("/data_types/list")
+def list():
+    out = data_types.select_by_type(name="List")
+    return out
+
+@app.get("/data_types/dictionary")
+def dictionary():
+    out = data_types.select_by_type(name="Dictionary")
+    return out
+
+@app.get("/data_types/tuple")
+def tuple():
+    out = data_types.select_by_type(name="Tuple")
+    return out
+
+@app.get("/data_types/summary")
+def summary():
+    return "Summary"
+
+@app.get("/about")
+def about():
+    return "About me"
 
 
+# ---------- POST ------------
 @app.post("/data_types")
 def create_data_type():
     # dt_body = data_type body
@@ -34,6 +64,7 @@ def create_data_type():
     return "", 204
 
 
+# ---------- PUT ------------
 @app.put("/data_types/<int:pk>")
 def update_data_type(pk):
     dt_body = request.json
@@ -41,6 +72,7 @@ def update_data_type(pk):
     return "", 204
 
 
+# ---------- DELETE ------------
 @app.delete("/data_types/<int:pk>")
 def delete_data_type(pk):
     data_types.delete(pk)
--- setup.sql
+++ setup.sql
@@ -14,7 +14,7 @@ INSERT INTO data_type (
 ) VALUES (
     "Integer",
     "Integer values",
-    "A data type that stores a whole number"
+    "A data type that stores a whole number."
 );
 
 INSERT INTO data_type (
@@ -24,7 +24,7 @@ INSERT INTO data_type (
 ) VALUES (
     "Float",
     "Floating point values",
-    "A data type that stores a number with decimal points"
+    "A data type that stores a number with decimal points."
 );
 
 INSERT INTO data_type (
@@ -34,5 +34,45 @@ INSERT INTO data_type (
 ) VALUES (
     "Boolean",
     "True or false values",
-    "Stores a True or False value, they also can be represented by 1 or 0 respectively"
+    "Stores a True or False value, they also can be represented by 1 or 0 respectively."
+);
+
+INSERT INTO data_type (
+    name,
+    summary,
+    description
+) VALUES (
+    "String",
+    "Text values",
+    "A data type that stores a chain of characters."
+);
+
+INSERT INTO data_type (
+    name,
+    summary,
+    description
+) VALUES (
+    "List",
+    "Collection data type",
+    "It can store multiple items into a single variable for ease of access, lists are created with []. They are ordered, changeable and allows duplicate values."
+);
+
+INSERT INTO data_type (
+    name,
+    summary,
+    description
+) VALUES (
+    "Dictionary",
+    "Collection data type",
+    "Stores multiple data values in key:value pairs, dictionaries are created with {}. They are unordered, changeable and don't allow duplicates."
+);
+
+INSERT INTO data_type (
+    name,
+    summary,
+    description
+) VALUES (
+    "Tuple",
+    "Collection data type",
+    "Stores multiple items in a single variable, they are created with (). They are ordered, unchangeable and allow duplicate values."
 );
